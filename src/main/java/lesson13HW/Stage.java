eaf77f03661b94a526f3496f87e28a45c414df01
sitnichuk91 <sitnichuk91@yandex.ru>
2022-04-05 16:11:07 +0700

    Initial commit

---
+++ src/main/java/lesson13HW/Stage.java
@@ -0,0 +1,10 @@
+package lesson13HW;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
+}
