d3b479ec8d6a43d97d5b575984c0cefc253eaa58
sitnichuk91 <sitnichuk91@yandex.ru>
2022-04-06 18:26:12 +0700

    Lesson13HW

--- src/main/java/lesson13HW/Car.java
+++ src/main/java/lesson13HW/Car.java
@@ -1,33 +1,54 @@
 package lesson13HW;
 
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private final List<Car> winnerList;
     private Race race;
     private int speed;
-    private String name;
+    public String name;
+    public CountDownLatch cdl;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CountDownLatch cdl, List<Car> winnerList) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cdl = cdl;
+        this.winnerList = winnerList;
+
     }
+
     @Override
     public void run() {
+
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            cdl.countDown();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        winnerList.add(this);
     }
 }
